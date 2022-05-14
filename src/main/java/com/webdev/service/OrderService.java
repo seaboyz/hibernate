b10b86abaf6386a6c626d3126a195e1c886fed3c
seaboyz <seaboyz@hotmail.com>
2022-05-14 17:52:10 -0500

    Add serivices

--- src/main/java/com/webdev/service/OrderService.java
+++ src/main/java/com/webdev/service/OrderService.java
@@ -1,26 +1,57 @@
 package com.webdev.service;
 
-import com.webdev.dao.AddressDao;
-import com.webdev.dao.CustomerDao;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 import com.webdev.dao.OrderDao;
-import com.webdev.dao.OrderItemDao;
-import com.webdev.dao.ProductDao;
+import com.webdev.model.Customer;
+import com.webdev.model.Order;
+import com.webdev.model.OrderItem;
+import com.webdev.model.Product;
+import com.webdev.model.ShippingAddress;
 
 public class OrderService {
-    private AddressDao addressDao;
-    private CustomerDao customerDao;
     private OrderDao orderDao;
-    private ProductDao productDao;
-    private OrderItemDao orderItemDao;
+    private CustomerService customerService;
+    private ProductService productService;
+    private AddressService addressService;
 
-    public OrderService(AddressDao addressDao, CustomerDao customerDao, OrderDao orderDao, ProductDao productDao,
-            OrderItemDao orderItemDao) {
-        this.addressDao = addressDao;
-        this.customerDao = customerDao;
+    public OrderService(OrderDao orderDao, CustomerService customerService, ProductService productService,
+            AddressService addressService) {
         this.orderDao = orderDao;
-        this.productDao = productDao;
-        this.orderItemDao = orderItemDao;
+        this.customerService = customerService;
+        this.productService = productService;
     }
 
-    // public placeAOrder(Integer customerId, Integer addressId)
+    // what is the minimum information needed to create an order?
+    // 1. Customer(id)
+    // 2. Address(address)
+    // 3. Product(id)
+    // 4. Quantity(int)
+
+    public Integer placeAOrder(Integer customerId, ShippingAddress shippingAddress,
+            HashMap<Integer, Integer> products) {
+
+        Customer customer = customerService.getCustomerById(customerId);
+
+        // convert products to OrderItemList
+        Set<OrderItem> orderItemList = new HashSet<>();
+
+        // 3. create an orderItem list
+        products.forEach((productId, quantity) -> {
+            Product product = productService.getProductById(productId);
+            orderItemList.add(new OrderItem(product, quantity));
+        });
+        // 4. create a new order using the customer and orderItemList and the address
+        Order order = new Order(customer, shippingAddress, orderItemList);
+        // 5. save shipping address as address to address list belongs to the customer
+        // in the database
+        addressService.addAddressToCustomer(customerId, shippingAddress);
+        // 6. save the order to the database
+        Integer orderId = orderDao.add(order);
+        // 7. return the orderId
+        return orderId;
+    }
+
 }
--- src/main/java/com/webdev/utils/AddressConverter.java
+++ src/main/java/com/webdev/utils/AddressConverter.java
@@ -1,9 +1,10 @@
 package com.webdev.utils;
 
+import com.webdev.model.Address;
 import com.webdev.model.ShippingAddress;
 
 public class AddressConverter {
-    public static ShippingAddress createShippingAddress(String firstName, String lastName,
+    public static ShippingAddress addressToShippingAddress(String firstName, String lastName,
             com.webdev.model.Address address) {
         return new ShippingAddress(
                 firstName,
@@ -15,5 +16,17 @@ public class AddressConverter {
                 address.getZip(),
                 address.getCountry());
 
+    }
+
+    public static Address shippingAddressToAddress(ShippingAddress address) {
+        return new Address(
+                address.getFirstName(),
+                address.getLastName(),
+                address.getStreet(),
+                address.getStreet2(),
+                address.getCity(),
+                address.getState(),
+                address.getZip(),
+                address.getCountry());
     }
 }
